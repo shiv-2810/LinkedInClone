1f5a39a44d37e2e3036c5c36ac314b740b85770d
shiv-2810 <s9431494772@gmail.com>
2022-07-11 11:08:17 +0530

    Initial app data

---
+++ linkedin/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+  return (
+    <div className='header'>Header
+    
+      <div className="header_left">
+         <img src="" alt="" />
+
+         <div className="header_search">
+            
+         </div>
+      </div>
+
+      <div className="header_right">
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
